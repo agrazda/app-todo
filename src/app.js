7729ba78b46d71d4d70ab68b74a434c79c4e4b2a
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-12-06 22:29:13 -0800

    lab 34

--- src/app.js
+++ src/app.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import ToDo from './components/todo/todo.js';
-import People from './components/people/People';
+import ToDo from "./components/todo/todo.js";
+import People from "./components/people/People";
+import Auth from './components/auth/auth.js';
+import Login from './components/auth/login.js';
+import LoginContext from './components/auth/context.js';
 
 export default class App extends React.Component {
   render() {
     return (
-    <>  
-      <ToDo />
-      <People />
-    </>  
+      <>
+        <ToDo />
+        <People />
+        <LoginContext>
+          <Login />
+
+          <Auth>
+            <div>Any valid user can see this</div>
+          </Auth>
+
+          <Auth capability="create">
+            <div>Users with create access can see this</div>
+          </Auth>
+
+          <Auth capability="update">
+            <div>Users with update access can see this</div>
+          </Auth>
+
+          <Auth capability="delete">
+            <div>Users with delete access can see this</div>
+          </Auth>
+        </LoginContext>
+      </>
     );
   }
 }
+
+module.export = App
